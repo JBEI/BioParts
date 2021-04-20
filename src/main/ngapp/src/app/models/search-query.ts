e6b08a1f5d94d0006ee8f9a732ea826d0672a54c
Hector Plahar <haplahar@lbl.gov>
2021-04-19 18:48:59 -0700

    moving paging to component of query object

--- src/main/ngapp/src/app/models/search-query.ts
+++ src/main/ngapp/src/app/models/search-query.ts
@@ -1,11 +1,16 @@
 import {BlastQuery} from "./blast-query";
+import {Paging} from "./paging";
 
 export class SearchQuery {
     queryString: string;
+    annotationSource: string;
     blastQuery: BlastQuery;
+    parameters: Paging;
 
     constructor() {
-        this.queryString = 'test';
+        this.queryString = '';
         this.blastQuery = new BlastQuery();
+        this.parameters = new Paging();
+        this.annotationSource = 'ALL';
     }
 }
